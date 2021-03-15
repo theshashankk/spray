6ed3ac2cca2fba1f3cfb7da716311ffbde56e0ab
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-03-15 23:25:44 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -19,12 +19,12 @@ from helpers.errors import DurationLimitError
 async def play(client: Client, message_: Message):
     audio = (message_.reply_to_message.audio or message_.reply_to_message.voice) if message_.reply_to_message else None
 
-    res = await message_.reply_text("CheemsBot=🔄 Processing...")
+    res = await message_.reply_text("ĆĦ€€ΜŞβØŦ=🔄 Processing...")
 
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
-                f"CheemsBot=Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
+                f"ĆĦ€€ΜŞβØŦ=Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
             )
 
         file_name = audio.file_id + audio.file_name.split(".")[-1]
@@ -50,7 +50,7 @@ async def play(client: Client, message_: Message):
                         break
 
         if offset == None:
-            await res.edit_text("CheemsBot=❕ You did not give me anything to play.")
+            await res.edit_text("ĆĦ€€ΜŞβØŦ=❕ You did not give me anything to play.")
             return
 
         url = text[offset:offset+length]
@@ -64,7 +64,7 @@ async def play(client: Client, message_: Message):
 
     if is_playing:
         position = await sira.add(message_.chat.id, file_path)
-        await res.edit_text(f"CheemsBot=#️⃣ Queued at position {position}.")
+        await res.edit_text(f"ĆĦ€€ΜŞβØŦ=#️⃣ Queued at position {position}.")
     else:
-        await res.edit_text("CheemsBot=▶️ Playing...")
+        await res.edit_text("ĆĦ€€ΜŞβØŦ=▶️ Playing...")
         tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path, 48000)
