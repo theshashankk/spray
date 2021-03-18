7d1f2c1857aef87364331db9ba00b762ad674451
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-03-18 08:47:11 +0530

    Update queue.py

--- sira/queue.py
+++ sira/queue.py
@@ -1,36 +1,54 @@
 """
-To prevent conflict with built-in modules, using "queues", the Greek word for "queue".
+To prevent conflict with built-in modules, using "sira", the Greek word for "queue".
 """
-from queue import Queue, Empty
+from asyncio.queues import QueueEmpty
 from typing import Dict, Union
+import asyncio
 
-queues: Dict[int, Queue] = {}
+
+queues: Dict[str, asyncio.Queue] = {}
 
 
-def add(chat_id: int, file_path: str) -> int:
+async def add(chat_id: Union[str, int], file_path: str) -> int:
+    if isinstance(chat_id, int):
+        chat_id = str(chat_id)
+
     if chat_id not in queues:
-        queues[chat_id] = Queue()
+        queues[chat_id] = asyncio.Queue()
 
-    queues[chat_id].put({"file_path": file_path})
-    return queues[chat_id].qsize()
+    await queues[chat_id].put(
+        {
+            "file_path": file_path
+        }
+    )
+    return queues[str(chat_id)].qsize()
 
 
-def get(chat_id: int) -> Union[Dict[str, str], None]:
+def get(chat_id: Union[str, int]) -> Union[Dict[str, str], None]:
+    if isinstance(chat_id, int):
+        chat_id = str(chat_id)
+
     if chat_id in queues:
         try:
             return queues[chat_id].get_nowait()
-        except Empty:
+        except asyncio.queues.QueueEmpty:
             return None
 
 
-def is_empty(chat_id: int) -> Union[bool, None]:
+def is_empty(chat_id: Union[str, int]) -> Union[bool, None]:
+    if isinstance(chat_id, int):
+        chat_id = str(chat_id)
+
     if chat_id in queues:
         return queues[chat_id].empty()
     else:
         return True
 
 
-def task_done(chat_id: int) -> None:
+def task_done(chat_id: Union[str, int]) -> None:
+    if isinstance(chat_id, int):
+        chat_id = str(chat_id)
+
     if chat_id in queues:
         try:
             queues[chat_id].task_done()
@@ -38,11 +56,14 @@ def task_done(chat_id: int) -> None:
             pass
 
 
-def clear(chat_id: int):
+def clear(chat_id: Union[str, int]) -> None:
+    if isinstance(chat_id, int):
+        chat_id = str(chat_id)
+
     if chat_id in queues:
         if queues[chat_id].empty():
-            raise Empty
+            raise QueueEmpty("The queue is empty.")
         else:
-            queues[chat_id].queue = []
+            queues[chat_id]._queue = []
     else:
-        raise Empty
+        raise QueueEmpty("The queue is empty.")
