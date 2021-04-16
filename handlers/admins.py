70413a9ce2545ace4e13f0b550b75076513491b1
Shashank sharma <78837537+theshashankk@users.noreply.github.com>
2021-04-16 17:32:09 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -16,7 +16,7 @@ from helpers.wrappers import errors, admins_only
 @admins_only
 async def pause(client: Client, message: Message):
     tgcalls.pytgcalls.pause_stream(message.chat.id)
-    await message.reply_text("ĆĦ€€ΜŞβØŦ=▶️ Paused.")
+    await message.reply_text("✯sтαя_вσт✯=▶️ Paused.")
 
 
 @Client.on_message(
@@ -28,7 +28,7 @@ async def pause(client: Client, message: Message):
 @admins_only
 async def resume(client: Client, message: Message):
     tgcalls.pytgcalls.resume_stream(message.chat.id)
-    await message.reply_text("ĆĦ€€ΜŞβØŦ=⏸ Resumed.")
+    await message.reply_text("✯sтαя_вσт✯=⏸ Resumed.")
 
 
 @Client.on_message(
@@ -45,7 +45,7 @@ async def stop(client: Client, message: Message):
         pass
 
     tgcalls.pytgcalls.leave_group_call(message.chat.id)
-    await message.reply_text("ĆĦ€€ΜŞβØŦ=⏹ Stopped streaming.")
+    await message.reply_text("✯sтαя_вσт✯=⏹ Stopped streaming.")
 
 
 @Client.on_message(
@@ -67,7 +67,7 @@ async def skip(client: Client, message: Message):
             chat_id, sira.get(chat_id)["file_path"]
         )
 
-    await message.reply_text("ĆĦ€€ΜŞβØŦ=⏩ Skipped the current song.")
+    await message.reply_text("✯sтαя_вσт✯=⏩ Skipped the current song.")
 
 
 @Client.on_message(
@@ -77,4 +77,4 @@ async def skip(client: Client, message: Message):
 @admins_only
 async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
-    await message.reply_text("ĆĦ€€ΜŞβØŦ=❇️ Admin cache refreshed!")
+    await message.reply_text("✯sтαя_вσт✯=❇️ Admin cache refreshed!")
